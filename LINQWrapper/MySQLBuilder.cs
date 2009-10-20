a03b271ec7be15989eca87efa70f7556f798fe14
Tim Martin <tim@asymptotic.co.uk>
2009-10-21 00:47:26 +0100

    Began planning classes for building SQL statements

---
+++ LINQWrapper/MySQLBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQWrapper
+{
+    /// <summary>
+    /// Builds SQL strings for the MySQL dialect
+    /// </summary>
+    public class MySQLBuilder : SQLBuilder
+    {
+    }
+}
---
+++ LINQWrapper/SQLBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQWrapper
+{
+    /// <summary>
+    /// Class that acts a little like StringBuilder, but for building SQL. It copes with the
+    /// various parts of the expression being added out of order.
+    /// </summary>
+    public interface SQLBuilder
+    {
+    }
+}
