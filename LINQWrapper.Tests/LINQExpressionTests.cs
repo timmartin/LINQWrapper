d5290b709b2b201d1d047362196c888c446acd57
Tim Martin <tim@asymptotic.co.uk>
2009-10-22 00:08:24 +0100

    Stub implementation of the query provider for LINQ

---
+++ LINQWrapper.Tests/LINQExpressionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using IQToolkit;
+using LINQWrapper;
+
+namespace LINQWrapper.Tests
+{
+    [TestFixture]
+    class LINQExpressionTests
+    {
+        /// <summary>
+        /// Check that we can write a LINQ expression - I'll be happy just to get it to <em>compile</em>
+        /// at the minute.
+        /// </summary>
+        [Test]
+        public void BasicExpressionTest()
+        {
+            LazyDBQueryProvider provider = new LazyDBQueryProvider();
+
+            Query<int> myQuery = new Query<int>(provider);
+
+            Assert.Throws(typeof(NotImplementedException), () => myQuery.Count());
+        }
+    }
+}
---
+++ LINQWrapper/LazyDBQueryProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+using IQToolkit;
+
+namespace LINQWrapper
+{
+    public class LazyDBQueryProvider : QueryProvider
+    {
+        public override string GetQueryText(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
