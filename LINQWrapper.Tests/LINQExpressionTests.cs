610b4a3d048c8f8e55b6d435cfc6eaa40c21d42b
Tim Martin <tim@asymptotic.co.uk>
2009-10-22 15:26:21 +0100

    The ObjectBuilder can now correctly set int and string properties on the objects it builds

--- LINQWrapper.Tests/LINQExpressionTests.cs
+++ LINQWrapper.Tests/LINQExpressionTests.cs
@@ -17,8 +17,8 @@ namespace LINQWrapper.Tests
     class LINQExpressionTests
     {
         /// <summary>
-        /// Check that we can write a LINQ expression - I'll be happy just to get it to <em>compile</em>
-        /// at the minute.
+        /// Check that we can execute an existing SQL expression within the LINQ wrapper, without
+        /// worrying about modifying the expression via LINQ (yet)
         /// </summary>
         [Test]
         public void BasicExpressionTest()
@@ -46,13 +46,37 @@ namespace LINQWrapper.Tests
                 .Method("Read")
                 .Will(Return.Value(true));
 
-            Expect.Exactly(4).On(mockReader)
+            Expect.Once.On(mockReader)
                 .Get["id"]
-                .Will(Return.Value("42"));
+                .Will(Return.Value("0"));
 
-            Expect.Exactly(4).On(mockReader)
+            Expect.Once.On(mockReader)
                 .Get["name"]
-                .Will(Return.Value("Bob Smith"));
+                .Will(Return.Value("Alice"));
+
+            Expect.Once.On(mockReader)
+                .Get["id"]
+                .Will(Return.Value("1"));
+
+            Expect.Once.On(mockReader)
+                .Get["name"]
+                .Will(Return.Value("Bob"));
+
+            Expect.Once.On(mockReader)
+                .Get["id"]
+                .Will(Return.Value("2"));
+
+            Expect.Once.On(mockReader)
+                .Get["name"]
+                .Will(Return.Value("Charles"));
+
+            Expect.Once.On(mockReader)
+                .Get["id"]
+                .Will(Return.Value("3"));
+
+            Expect.Once.On(mockReader)
+                .Get["name"]
+                .Will(Return.Value("Dan"));
 
             Expect.Once.On(mockReader)
                 .Method("Read")
@@ -70,6 +94,16 @@ namespace LINQWrapper.Tests
             List<Employee> resultList = myQuery.ToList();
 
             Assert.AreEqual(4, resultList.Count);
+
+            Assert.AreEqual(0, resultList[0].ID);
+            Assert.AreEqual(1, resultList[1].ID);
+            Assert.AreEqual(2, resultList[2].ID);
+            Assert.AreEqual(3, resultList[3].ID);
+
+            Assert.AreEqual("Alice", resultList[0].Name);
+            Assert.AreEqual("Bob", resultList[1].Name);
+            Assert.AreEqual("Charles", resultList[2].Name);
+            Assert.AreEqual("Dan", resultList[3].Name);
         }
     }
 }
--- LINQWrapper/ObjectBuilder.cs
+++ LINQWrapper/ObjectBuilder.cs
@@ -10,6 +10,13 @@ using LINQWrapper.DBMapping;
 
 namespace LINQWrapper
 {
+    /// <summary>
+    /// Maps a data reader object into an enumeration of proper .Net classes. Technically
+    /// this ought to be called something like ObjectCollectionBuilder, since it builds
+    /// more than one object. This does mapping with reflection and special attributes on the
+    /// type, but ought not to have anything to do with the LINQ code per se.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal class ObjectBuilder<T> : IEnumerable<T>, IEnumerable where T : class, new()
     {
         public ObjectBuilder(IDataReader reader)
@@ -23,6 +30,11 @@ namespace LINQWrapper
             }
         }
 
+        /// <summary>
+        /// Construct a single object of type T from a single row in the data reader.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
         private T MakeObject(IDataReader reader)
         {
             T obj = new T();
@@ -37,7 +49,20 @@ namespace LINQWrapper
             {
                 string fieldName = ((FieldMappingAttribute)property.GetCustomAttributes(typeof(FieldMappingAttribute), false).First()).FieldName;
 
-                string value = reader[fieldName].ToString();
+                Type targetType = property.PropertyType;
+
+                if (targetType == typeof(int))
+                {
+                    int value = int.Parse(reader[fieldName].ToString());
+
+                    property.SetValue(obj, value, null);
+                }
+                else if (targetType == typeof(string))
+                {
+                    string value = reader[fieldName].ToString();
+
+                    property.SetValue(obj, value, null);
+                }
             }
 
             return obj;
